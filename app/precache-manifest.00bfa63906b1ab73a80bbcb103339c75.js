d1250994cba084fb54b52794fbfa0fb49094d17e
Wally Atkins <wallyatkins@gmail.com>
2021-01-12 09:11:53 -0500

    testing 'app' homepage setting

---
+++ app/precache-manifest.00bfa63906b1ab73a80bbcb103339c75.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6ce7965287fe41d6330389ee3e1707",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "f38adc70b5d493bf8bd0",
+    "url": "/app/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "05f4972e7a75ba55179d",
+    "url": "/app/static/css/main.3e3ef894.chunk.css"
+  },
+  {
+    "revision": "f38adc70b5d493bf8bd0",
+    "url": "/app/static/js/2.f5b71a91.chunk.js"
+  },
+  {
+    "revision": "bd051b41e1553fa898776b07c258cd8b",
+    "url": "/app/static/js/2.f5b71a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f4972e7a75ba55179d",
+    "url": "/app/static/js/main.c18bb3a6.chunk.js"
+  },
+  {
+    "revision": "347e22d62af1ecdda7cf",
+    "url": "/app/static/js/runtime-main.4a922534.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/app/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/app/static/media/logo.ee7cd8ed.svg"
+  }
+]);
