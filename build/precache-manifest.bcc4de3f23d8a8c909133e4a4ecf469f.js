2db5c45583ae0fb17fc36670ce3469e138c7268f
Wally Atkins <wallyatkins@gmail.com>
2021-01-11 22:07:37 -0500

    test of auth0 react

---
+++ build/precache-manifest.bcc4de3f23d8a8c909133e4a4ecf469f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c884b539ed941ab9a0d64dc9291fb05e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f38adc70b5d493bf8bd0",
+    "url": "/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "ddc845b9fd1203bd484e",
+    "url": "/static/css/main.3e3ef894.chunk.css"
+  },
+  {
+    "revision": "f38adc70b5d493bf8bd0",
+    "url": "/static/js/2.f5b71a91.chunk.js"
+  },
+  {
+    "revision": "bd051b41e1553fa898776b07c258cd8b",
+    "url": "/static/js/2.f5b71a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddc845b9fd1203bd484e",
+    "url": "/static/js/main.01d4329e.chunk.js"
+  },
+  {
+    "revision": "0231f95ac667b73c9187",
+    "url": "/static/js/runtime-main.91e21c86.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
